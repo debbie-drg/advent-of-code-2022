41385af36de2f8e7146b328a15a8926b1771687c
Deivi Dragon <deivi.dragon.9@gmail.com>
2022-12-02 16:11:14 +0000

    Sorting with sorted

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,8 +1,7 @@
 with open("input.txt") as f:
     text = f.read().split(sep = "\n\n")
     text = [[int(item) for item in sublist.split("\n") if item != ""] for sublist in text]
-    calories = list(map(sum, text))
-calories.sort(reverse = True)
+    calories = sorted(map(sum, text), reverse = True)
 
 print("Top: ", calories[0])
 print("Top 3: ", sum(calories[:3]))
