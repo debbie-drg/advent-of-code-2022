ea097eed5caab404ecb760173d7dc2e13562668d
Deivi Dragon <deivi.dragon.9@gmail.com>
2022-12-03 18:22:55 +0000

    Solution in Rust.

---
+++ Day1/day1.rs
@@ -0,0 +1,19 @@
+use std::fs::File;
+use std::io::Read;
+
+fn load_file_split_strings(file_name: &str) -> String {
+    let mut file = File::open(file_name).expect("File not found.");
+    let mut data = String::new();
+    file.read_to_string(&mut data).expect("File not found.");
+    return data;
+}
+
+fn main() {
+    let data = load_file_split_strings("input.txt");
+    let mut calories: Vec<_> = data.split("\n\n").map(|element| element.lines().map(|item| item.parse::<u32>().unwrap()).sum::<u32>()).collect();
+    calories.sort();
+    calories.reverse();
+    println!("Top: {}", calories[0]);
+    let biggest_three = calories[0..3].iter().sum::<u32>();
+    println!("Top three: {}", biggest_three)
+}
