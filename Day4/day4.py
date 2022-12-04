9823c8d729f102d73d1bc677f4b16db27a29abfc
Deivi Dragon <deivi.dragon.9@gmail.com>
2022-12-04 15:44:22 +0000

    Improved prints

--- Day4/day4.py
+++ Day4/day4.py
@@ -27,5 +27,5 @@ if __name__ == "__main__":
     score_round_1 = sum(map(intervals_contained, interval_extremes_1))
     score_round_2 = sum(map(intervals_overlap, interval_extremes_2))
 
-    print(f"The number of overlaps in round one is {score_round_1}")
+    print(f"The number of contained intervals is {score_round_1}")
     print(f"The number of overlaps in round two is {score_round_2}")
