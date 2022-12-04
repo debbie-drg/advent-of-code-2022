6f93b2b784ec4c0e4a43222670f18d92e7daee3e
Deivi Dragon <deivi.dragon.9@gmail.com>
2022-12-04 09:25:19 +0000

    Format changes

--- Day4/day4.py
+++ Day4/day4.py
@@ -1,18 +1,30 @@
 from itertools import tee
 
+
 def intervals_contained(intervals: list[list[int]]) -> bool:
-    return (intervals[0][0] - intervals[1][0]) * (intervals[0][1] - intervals[1][1]) <= 0
+    return (intervals[0][0] - intervals[1][0]) * (
+        intervals[0][1] - intervals[1][1]
+    ) <= 0
+
 
 def intervals_overlap(intervals: list[list[int]]) -> bool:
-    return max(intervals[0][0], intervals[1][0]) <= min(intervals[0][1], intervals[1][1])
+    return max(intervals[0][0], intervals[1][0]) <= min(
+        intervals[0][1], intervals[1][1]
+    )
+
 
 def split_sections_into_intervals(string: str) -> list[list[int]]:
-    return [[int(number) for number in item.split("-")] for item in string.split(sep = ",")]
+    return [
+        [int(number) for number in item.split("-")] for item in string.split(sep=",")
+    ]
+
 
 with open("input.txt") as f:
-    data = f.read().split(sep = "\n")
+    data = f.read().split(sep="\n")
     data.remove("")
-    interval_extremes_1, interval_extremes_2 = tee(map(split_sections_into_intervals, data), 2)
+    interval_extremes_1, interval_extremes_2 = tee(
+        map(split_sections_into_intervals, data), 2
+    )
     score_round_1 = sum(map(intervals_contained, interval_extremes_1))
     score_round_2 = sum(map(intervals_overlap, interval_extremes_2))
 
