847a8ad9acc9bc231924934d79ad3f8fa044a53e
Deivi Dragon <deivi.dragon.9@gmail.com>
2022-12-05 10:22:31 +0000

    Done

--- Day5/day5.py
+++ Day5/day5.py
@@ -1,19 +1,34 @@
 from copy import deepcopy
 
 
-def extract_stacks(cranes: str) -> dict():
-    cranes = cranes.split("\n")
-    stack_values = [element for element in cranes[-1] if element != " "]
-    stacks = {key: [] for key in stack_values}
-    positions = [cranes[-1].index(stack) for stack in stacks]
+class Port:
+    def __init__(self, cranes: str, mode_9001: bool = False):
+        cranes = cranes.split("\n")
+        stack_values = [element for element in cranes[-1] if element != " "]
+        stacks = {key: [] for key in stack_values}
+        positions = [cranes[-1].index(stack) for stack in stacks]
 
-    for line in cranes[-2::-1]:
-        for index, element in enumerate(stacks):
-            contents = line[positions[index]]
-            if contents != " ":
-                stacks[element].append(line[positions[index]])
+        for line in cranes[-2::-1]:
+            for index, element in enumerate(stacks):
+                contents = line[positions[index]]
+                if contents != " ":
+                    stacks[element].append(line[positions[index]])
 
-    return stacks
+        self.status = stacks
+        self.mode_9001 = mode_9001
+
+    def move(self, number: int, move_from: str, move_to: str):
+        elements_to_move = self.status[move_from][-number:]
+        del self.status[move_from][-number:]
+        if not self.mode_9001:
+            elements_to_move.reverse()
+        self.status[move_to] += elements_to_move
+
+    def print_top(self) -> str:
+        top_elements = ""
+        for key in self.status:
+            top_elements += self.status[key][-1]
+        return top_elements
 
 
 def extract_moves(moves: str) -> list:
@@ -24,33 +39,19 @@ def extract_moves(moves: str) -> list:
     return moves
 
 
-def move(moves: list, stacks: dict, reverse: bool = True):
-    for move in moves:
-        elements_to_move = stacks[move[1]][-move[0] :]
-        del stacks[move[1]][-move[0] :]
-        if reverse:
-            elements_to_move.reverse()
-        stacks[move[2]] += elements_to_move
-
-
-def print_top(stacks: dict) -> str:
-    top_elements = ""
-    for key in stacks:
-        top_elements += stacks[key][-1]
-    return top_elements
-
-
 if __name__ == "__main__":
     cranes, moves = open("input.txt").read().split(sep="\n\n")
     moves = extract_moves(moves)
-    stacks_9000 = extract_stacks(cranes)
+    stacks_9000 = Port(cranes)
     stacks_9001 = deepcopy(stacks_9000)
-    move(moves, stacks_9000)
-    move(moves, stacks_9001, reverse=False)
+    stacks_9001.mode_9001 = True
+    for move in moves:
+        stacks_9000.move(*move)
+        stacks_9001.move(*move)
 
     print(
-        f"The elements at the top after moving with CrateMover 9000 are {print_top(stacks_9000)}"
+        f"The elements at the top after moving with CrateMover 9000 are {stacks_9000.print_top()}"
     )
     print(
-        f"The elements at the top after moving with CrateMover 9001 are {print_top(stacks_9001)}"
+        f"The elements at the top after moving with CrateMover 9001 are {stacks_9001.print_top()}"
     )
