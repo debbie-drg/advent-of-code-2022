59519ad2b8892e726a6040d8d12f62946f3bc35f
Deivi Dragon <deivi.dragon.9@gmail.com>
2022-12-06 22:33:01 +0000

    Added new, more efficient solution.

--- Day6/day6.rs
+++ Day6/day6.rs
@@ -1,3 +1,5 @@
+const VALUE_A: u8 = b'a';
+
 fn get_data() -> String {
     let input_args: Vec<String> = std::env::args().collect();
     let mut file_name: String = "input.txt".to_string();
@@ -11,8 +13,9 @@ fn get_data() -> String {
     return data;
 }
 
-fn detect_first_marker(datastream: &str, length_of_indicator: usize) -> usize {
-    return datastream.as_bytes()
+fn _detect_first_marker_old(datastream: &str, length_of_indicator: usize) -> usize {
+    return datastream
+        .as_bytes()
         .windows(length_of_indicator)
         .position(|window| {
             window
@@ -21,7 +24,51 @@ fn detect_first_marker(datastream: &str, length_of_indicator: usize) -> usize {
                 .all(|(idx, c)| !window[..idx].contains(c))
         })
         .unwrap()
-        + length_of_indicator
+        + length_of_indicator;
+}
+
+fn detect_first_marker(datastream: &str, length_of_indicator: usize) -> usize {
+    // We use a sliding window
+    let mut counts = [0u32; 26];
+    let mut unique = 0;
+
+    let bytesstream = datastream.as_bytes();
+
+    // We populate with the first letters, up to the length of the indicator
+    for index in 0..length_of_indicator {
+        let character = (bytesstream[index] - VALUE_A) as usize;
+
+        counts[character] += 1;
+        if counts[character] == 1 {
+            unique += 1;
+        }
+    }
+
+    if unique == length_of_indicator {
+        return length_of_indicator;
+    }
+
+    // Now we update the amount of unique elements and counts by a sliding window.
+
+    bytesstream
+        .windows(length_of_indicator + 1) // We add one to remove the leaving letter
+        .enumerate()
+        .find(|&(_index, window)| {
+            let character = (&window[0] - VALUE_A) as usize;
+            counts[character] -= 1;
+            if counts[character] == 0 {
+                unique -= 1;
+            }
+            let character = (&window[length_of_indicator] - VALUE_A) as usize;
+            counts[character] += 1;
+            if counts[character] == 1 {
+                unique += 1;
+            }
+
+            unique == length_of_indicator
+        })
+        .map(|(index, _window)| index + length_of_indicator + 1)
+        .unwrap()
 }
 
 fn main() {
