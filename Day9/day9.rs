c598bedee0a61bdfff568e1e7e9469a389861639
Deivi Dragon <deivi.dragon.9@gmail.com>
2022-12-09 23:21:33 +0000

    Added solution in Rust

---
+++ Day9/day9.rs
@@ -0,0 +1,116 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+fn get_data() -> String {
+    let input_args: Vec<String> = std::env::args().collect();
+    let mut file_name: String = "input.txt".to_string();
+    if input_args.len() > 1 {
+        file_name = input_args[1].clone();
+    }
+    let mut data = std::fs::read_to_string(file_name).unwrap();
+    if data.ends_with("\n") {
+        data.pop();
+    }
+    return data;
+}
+
+fn add_vectors(vector_1: &Vec<i32>, vector_2: &Vec<i32>) -> Vec<i32> {
+    let mut sum_vector = vec![];
+    sum_vector.push(vector_1[0] + vector_2[0]);
+    sum_vector.push(vector_1[1] + vector_2[1]);
+    sum_vector
+}
+
+fn substract_vectors(vector_1: &Vec<i32>, vector_2: &Vec<i32>) -> Vec<i32> {
+    let mut sum_vector = vec![];
+    sum_vector.push(vector_1[0] - vector_2[0]);
+    sum_vector.push(vector_1[1] - vector_2[1]);
+    sum_vector
+}
+
+fn grid_distance(head: &Vec<i32>, tail: &Vec<i32>) -> i32 {
+    return substract_vectors(head, tail)
+        .into_iter()
+        .map(|element| element.abs())
+        .max()
+        .unwrap();
+}
+
+fn make_ones(position: Vec<i32>) -> Vec<i32> {
+    let mut ones_vector = vec![];
+    if position[0] != 0 {
+        ones_vector.push(position[0] / position[0].abs());
+    } else {
+        ones_vector.push(0)
+    }
+    if position[1] != 0 {
+        ones_vector.push(position[1] / position[1].abs());
+    } else {
+        ones_vector.push(0)
+    }
+    ones_vector
+}
+
+fn move_step(
+    rope: &mut Vec<Vec<i32>>,
+    command: Vec<&str>,
+    positions_visited: &mut HashSet<Vec<i32>>,
+    move_correspondence: &HashMap<&str, Vec<i32>>,
+) {
+    let move_kind = command[0];
+    let move_amount = command[1].parse::<usize>().unwrap();
+    for _ in 0..move_amount {
+        rope[0] = add_vectors(&rope[0], &move_correspondence[move_kind]);
+        for index in 1..rope.len() {
+            if grid_distance(&rope[index - 1], &rope[index]) <= 1 {
+                break;
+            }
+            rope[index] = add_vectors(
+                &rope[index],
+                &make_ones(substract_vectors(&rope[index - 1], &rope[index])),
+            )
+        }
+        positions_visited.insert(rope[rope.len() - 1].clone());
+    }
+}
+
+fn move_all(
+    commands: &Vec<&str>,
+    rope_length: usize,
+    move_correspondence: &HashMap<&str, Vec<i32>>,
+) -> HashSet<Vec<i32>> {
+    let mut rope = vec![vec![0, 0]; rope_length];
+    let mut positions_visited: HashSet<Vec<i32>> = HashSet::new();
+    for command in commands {
+        let current_command = command.split(" ").collect();
+        move_step(
+            &mut rope,
+            current_command,
+            &mut positions_visited,
+            &move_correspondence,
+        );
+    }
+    return positions_visited;
+}
+
+fn main() {
+    let data = get_data();
+    let split_data = data.split("\n").collect();
+    let move_correspondence: HashMap<&str, Vec<i32>> = HashMap::from([
+        ("R", vec![1, 0]),
+        ("L", vec![-1, 0]),
+        ("U", vec![0, 1]),
+        ("D", vec![0, -1]),
+    ]);
+    let visited_part_1 = move_all(&split_data, 2, &move_correspondence);
+    println!(
+        "The number of points visited for size 2 is {}.",
+        visited_part_1.len()
+    );
+
+    let visited_part_2 = move_all(&split_data, 10, &move_correspondence);
+    println!(
+        "The number of points visited for size 10 is {}.",
+        visited_part_2.len()
+    )
+}
