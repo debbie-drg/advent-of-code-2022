6f2fd55a290d5baa669e77a5c715ee4f0d935c57
Deivi Dragon <deivi.dragon.9@gmail.com>
2022-12-09 23:21:15 +0000

    Made more readable

--- Day9/day9.py
+++ Day9/day9.py
@@ -9,15 +9,21 @@ MOVE_CORRESPONDENCE = {
 }
 
 
+def preprocess_commands(commands):
+    commands = commands.split("\n")
+    commands.remove("")
+    commands = [command.split(" ") for command in commands]
+    commands = [[command[0], int(command[1])] for command in commands]
+    return commands
+
 def grid_distance(head, tail):
     return np.max(np.abs(head - tail))
 
 
-def move(rope, command, positions_visited):
+def move_step(rope, command, positions_visited):
     global MOVE_CORRESPONDENCE
-    head_move = MOVE_CORRESPONDENCE[command[0]]
     for _ in range(command[1]):
-        rope[0] += head_move
+        rope[0] += MOVE_CORRESPONDENCE[command[0]]
         for index in range(1, rope.shape[0]):
             if grid_distance(rope[index - 1], rope[index]) <= 1:
                 break
@@ -26,26 +32,24 @@ def move(rope, command, positions_visited):
     return rope, positions_visited
 
 
+def move_all(commands, rope_length):
+    rope = np.zeros((rope_length,2), dtype=int)
+    positions_visited = {(0,0)}
+    for command in commands:
+        rope, positions_visited = move_step(rope, command, positions_visited)
+    return positions_visited
+
 if __name__ == "__main__":
     try:
         file_name = sys.argv[1]
     except IndexError:
         file_name = "input.txt"
 
-    rope_1 = np.zeros((2, 2), dtype=int)
-    positions_visited_1 = {(0, 0)}
+    commands = open(file_name).read()
+    commands = preprocess_commands(commands)
 
-    rope_2 = np.zeros((10, 2), dtype=int)
-    positions_visited_2 = {(0, 0)}
-
-    with open(file_name) as commands:
-        for command in commands:
-            command = command.split(" ")
-            command[1] = int(command[1])
-
-            rope_1, positions_visited_1 = move(rope_1, command, positions_visited_1)
-
-            rope_2, positions_visited_2 = move(rope_2, command, positions_visited_2)
+    positions_visited_1 = move_all(commands, 2)
+    positions_visited_2 = move_all(commands, 10)
 
     print(f"The tail visited {len(positions_visited_1)} positions in part one.")
     print(f"The tail visited {len(positions_visited_2)} positions in part two.")
