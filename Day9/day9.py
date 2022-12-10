41b417bf0b7a0279e3f1175f5492c82041e98337
Deivi Dragon <deivi.dragon.9@gmail.com>
2022-12-10 18:38:06 +0000

    Now not using numpy

--- Day9/day9.py
+++ Day9/day9.py
@@ -1,11 +1,10 @@
 import sys
-import numpy as np
 
 MOVE_CORRESPONDENCE = {
-    "R": np.array([1, 0], dtype=int),
-    "L": np.array([-1, 0], dtype=int),
-    "U": np.array([0, 1], dtype=int),
-    "D": np.array([0, -1], dtype=int),
+    "R": [1, 0],
+    "L": [-1, 0],
+    "U": [0, 1],
+    "D": [0, -1],
 }
 
 
@@ -16,28 +15,48 @@ def preprocess_commands(commands):
     commands = [[command[0], int(command[1])] for command in commands]
     return commands
 
+
 def grid_distance(head, tail):
-    return np.max(np.abs(head - tail))
+    return max([abs(head[0] - tail[0]), abs(head[1] - tail[1])])
+
+
+def add_vectors(vector_1: list, vector_2: list) -> list:
+    return [vector_1[index] + vector_2[index] for index in range(len(vector_1))]
+
+
+def substract_vectors(vector_1: list, vector_2: list) -> list:
+    return [vector_1[index] - vector_2[index] for index in range(len(vector_1))]
+
+
+def make_ones(vector: list) -> list:
+    ones_vector = [0] * len(vector)
+    for index in range(len(vector)):
+        try:
+            ones_vector[index] = vector[index]/abs(vector[index])
+        except ZeroDivisionError:
+            ones_vector[index] = 0
+    return ones_vector
 
 
 def move_step(rope, command, positions_visited):
     global MOVE_CORRESPONDENCE
     for _ in range(command[1]):
-        rope[0] += MOVE_CORRESPONDENCE[command[0]]
-        for index in range(1, rope.shape[0]):
+        rope[0] = add_vectors(rope[0], MOVE_CORRESPONDENCE[command[0]])
+        for index in range(1, len(rope)):
             if grid_distance(rope[index - 1], rope[index]) <= 1:
                 break
-            rope[index] += np.sign(rope[index - 1] - rope[index])
+            rope[index] = add_vectors(rope[index], make_ones(substract_vectors(rope[index-1], rope[index])))
             positions_visited.add(tuple(rope[-1]))
     return rope, positions_visited
 
 
 def move_all(commands, rope_length):
-    rope = np.zeros((rope_length,2), dtype=int)
-    positions_visited = {(0,0)}
+    rope = [[0,0] for _ in range(rope_length)]
+    positions_visited = {(0, 0)}
     for command in commands:
         rope, positions_visited = move_step(rope, command, positions_visited)
     return positions_visited
+
 
 if __name__ == "__main__":
     try:
