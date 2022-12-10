c17db59580fb141a295b9ad8d6ea29d5aeaa7730
Deivi Dragon <deivi.dragon.9@gmail.com>
2022-12-10 18:28:04 +0000

    Both parts now done at the same time

--- Day10/day10.rs
+++ Day10/day10.rs
@@ -11,28 +11,6 @@ fn get_data() -> String {
     return data;
 }
 
-fn signal_strength(instructions: &Vec<&str>) -> i32 {
-    let mut registry_x: i32 = 1;
-    let mut strength_sum: i32 = 0;
-    let mut cycle_count: i32 = 0;
-
-    for instruction in instructions {
-        let split_instruction: Vec<&str> = instruction.split(" ").collect();
-        cycle_count += 1;
-        if cycle_count.rem_euclid(40) == 20 {
-            strength_sum += cycle_count * registry_x
-        }
-        if split_instruction.len() == 2 {
-            cycle_count += 1;
-            if cycle_count.rem_euclid(40) == 20 {
-                strength_sum += cycle_count * registry_x;
-            }
-            registry_x += split_instruction[1].parse::<i32>().unwrap();
-        }
-    }
-    strength_sum
-}
-
 fn check_sprite(cycle_count: &i32, registry: &i32) -> bool {
     let position = cycle_count.rem_euclid(40);
     if (position == registry - 1) | (position == *registry) | (position == registry + 1) {
@@ -53,9 +31,10 @@ fn check_new_line(cycle_count: &i32) -> bool {
     return cycle_count.rem_euclid(40) == 0
 }
 
-fn render_display(instructions: &Vec<&str>) -> String {
+fn render_display(instructions: &Vec<&str>) -> (i32, String) {
     let mut display = String::new();
     let mut registry_x: i32 = 1;
+    let mut strength_sum: i32 = 0;
     let mut cycle_count: i32 = 0;
 
     for instruction in instructions {
@@ -65,27 +44,31 @@ fn render_display(instructions: &Vec<&str>) -> String {
         }
         display.push(pixel(check_sprite(&cycle_count, &registry_x)));
         cycle_count += 1;
+        if cycle_count.rem_euclid(40) == 20 {
+            strength_sum += cycle_count * registry_x
+        }
         if split_instruction.len() == 2 {
             if check_new_line(&cycle_count) == true {
                 display.push('\n');
             }
             display.push(pixel(check_sprite(&cycle_count, &registry_x)));
             cycle_count += 1;
+            if cycle_count.rem_euclid(40) == 20 {
+                strength_sum += cycle_count * registry_x
+            }
             registry_x += split_instruction[1].parse::<i32>().unwrap();
         }
     }
-    return display
+    return (strength_sum, display)
 }
 
 
 fn main() {
     let data = get_data();
     let split_data: Vec<&str> = data.split("\n").collect();
-    let strength = signal_strength(&split_data);
 
-    println!("The signal strength is {}", strength);
-
-    let display = render_display(&split_data);
+    let (strength_sum, display) = render_display(&split_data);
+    println!("The signal strength is {}", strength_sum);
     println!("\nThis is what the display looks like!");
     println!("{}", display)
 }
