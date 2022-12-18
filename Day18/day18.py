1ae4d890f9bf8b8acf30d6029802e2cc83fcaea8
Deivi Dragon <deivi.dragon.9@gmail.com>
2022-12-18 17:09:41 +0000

    Getting prepared for part 2.

--- Day18/day18.py
+++ Day18/day18.py
@@ -1,42 +1,39 @@
 import sys
 
-
-def parse_one_cube(cube: str) -> tuple[int]:
-    split_cube = cube.split(",")
-    return tuple(map(int, split_cube))
+NEIGHBOURS = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
 
 
-def number_neighbours(cube: tuple, other_cubes: list[tuple]):
-    neighbours = 0
-    if (cube[0] - 1, cube[1], cube[2]) in other_cubes:
-        neighbours += 1
-    if (cube[0] + 1, cube[1], cube[2]) in other_cubes:
-        neighbours += 1
-    if (cube[0], cube[1] - 1, cube[2]) in other_cubes:
-        neighbours += 1
-    if (cube[0], cube[1] + 1, cube[2]) in other_cubes:
-        neighbours += 1
-    if (cube[0], cube[1], cube[2] - 1) in other_cubes:
-        neighbours += 1
-    if (cube[0], cube[1], cube[2] + 1) in other_cubes:
-        neighbours += 1
-    return neighbours
+class Lava:
+    def __init__(self, cubes_input: str):
+        self.cubes = []
+        split_cubes = cubes_input.split("\n")
+        try:
+            split_cubes.remove("")
+        except ValueError:
+            pass
+        for cube in split_cubes:
+            self.add_cube(cube)
 
+    def add_cube(self, cube: str):
+        split_cube = cube.split(",")
+        self.cubes.append(tuple(map(int, split_cube)))
 
-def surface_area(cubes_list: list[tuple]) -> int:
-    surface_area = 6 * len(cubes_list)
-    for index in range(len(cubes_list) - 1):
-        surface_area -= 2 * number_neighbours(cubes_list[index], cubes_list[index + 1:])
-    return surface_area
+    def number_neighbours(self, cube: tuple) -> int:
+        neighbours = 0
+        for neighbour in NEIGHBOURS:
+            if (
+                cube[0] + neighbour[0],
+                cube[1] + neighbour[1],
+                cube[2] + neighbour[2],
+            ) in self.cubes:
+                neighbours += 1
+        return neighbours
 
-
-def parse_cubes(cubes_input: str) -> list[tuple[int]]:
-    split_cubes = cubes_input.split("\n")
-    try:
-        split_cubes.remove("")
-    except ValueError:
-        pass
-    return list(map(parse_one_cube, split_cubes))
+    def surface_area(self) -> int:
+        surface_area = 6 * len(self.cubes)
+        for cube in self.cubes:
+            surface_area -= self.number_neighbours(cube)
+        return surface_area
 
 
 if __name__ == "__main__":
@@ -44,6 +41,8 @@ if __name__ == "__main__":
         file_name = sys.argv[1]
     except IndexError:
         file_name = "input.txt"
-    cubes = parse_cubes(open(file_name).read())
 
-    print(f"The surface area is {surface_area(cubes)}.")
+    cubes_input = open(file_name).read()
+    lava = Lava(cubes_input)
+
+    print(f"The surface area is {lava.surface_area()}.")
