1910e340c4678eac13a7c072bfe7c3c630caa794
Deivi Dragon <deivi.dragon.9@gmail.com>
2022-12-18 18:24:54 +0000

    Part 2 done!

--- Day18/day18.py
+++ Day18/day18.py
@@ -1,4 +1,5 @@
 import sys
+sys.setrecursionlimit(10000)
 
 NEIGHBOURS = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
 
@@ -13,6 +14,8 @@ class Lava:
             pass
         for cube in split_cubes:
             self.add_cube(cube)
+        max_indices = (max(cube[i] for cube in self.cubes) for i in range(3))
+        self.max_x, self.max_y, self.max_z = max_indices
 
     def add_cube(self, cube: str):
         split_cube = cube.split(",")
@@ -35,6 +38,29 @@ class Lava:
             surface_area -= self.number_neighbours(cube)
         return surface_area
 
+    def outer_surface(self) -> int:
+        position = (-1,-1,-1)
+        surface, _ = self.flood_step(position, 0, [])
+        return surface
+
+    def flood_step(self, position: tuple[int, int, int], surface: int, checked: list[tuple[int, int, int]]):
+        if position in checked:
+            return surface, checked
+        if position in self.cubes:
+            return surface + 1, checked
+        checked.append(position)
+        if any(element < -1 for element in list(position)):
+            return surface, checked
+        if any([position[0] > self.max_x + 1, position[1] > self.max_y + 1, position[2] > self.max_z + 1]):
+            return surface , checked
+        for neighbour in NEIGHBOURS:
+            next_step = (
+                position[0] + neighbour[0],
+                position[1] + neighbour[1],
+                position[2] + neighbour[2],
+            )
+            surface, checked = self.flood_step(next_step, surface, checked)
+        return surface, checked
 
 if __name__ == "__main__":
     try:
@@ -46,3 +72,4 @@ if __name__ == "__main__":
     lava = Lava(cubes_input)
 
     print(f"The surface area is {lava.surface_area()}.")
+    print(f"The outer surface area is {lava.outer_surface()}.")
