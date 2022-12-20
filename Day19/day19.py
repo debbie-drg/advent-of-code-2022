0f9b733a29590be0ca7177f7a0de8d565b8fb05a
Deivi Dragon <deivi.dragon.9@gmail.com>
2022-12-20 15:12:22 +0000

    Moved to only tracking actions.

--- Day19/day19.py
+++ Day19/day19.py
@@ -1,6 +1,7 @@
 import sys
 from copy import copy
 import multiprocessing
+from math import ceil
 
 TIME_LIMIT = 24
 TIME_LIMIT_ELEPHANTS_NOT_HUNGRY = 32
@@ -30,9 +31,11 @@ class Blueprint:
         # Costs are codified through positions on list.
 
     @staticmethod
-    def update_materials(materials: list[int], number_robots: list[int]) -> list[int]:
+    def update_materials(
+        materials: list[int], number_robots: list[int], number_turns: int
+    ) -> list[int]:
         materials_after = [
-            material + number_robots
+            material + number_turns * number_robots
             for material, number_robots in zip(materials, number_robots)
         ]
         return materials_after
@@ -48,25 +51,30 @@ class Blueprint:
         ]
         return materials_after, number_robots_after
 
-    def enough_materials(
-        self, materials: list[int], number_robots: list[int], time_remaining: int
-    ) -> list[int]:
-        enough = []
-        for index in range(
-            len(self.robot_costs) - 1, -1, -1
-        ):  # Reverse to give later robots priority
-            if (
-                index == 3
-                or number_robots[index] * time_remaining + materials[index]
-                < time_remaining * self.max_cost_per_resource[index]
-            ) and all(
-                [
-                    material >= cost
-                    for material, cost in zip(materials, self.robot_costs[index])
-                ]
-            ):
-                enough.append(index)
-        return enough
+    def turns_to_purchase(
+        self,
+        materials: list[int],
+        number_robots: list[int],
+        time_remaining: int,
+        robot_type: int,
+    ) -> int:
+        if robot_type != 3 and (
+            number_robots[robot_type] * time_remaining + materials[robot_type]
+            >= time_remaining * self.max_cost_per_resource[robot_type]
+        ):
+            return -1  # Purchasing would be useless
+        turns_to_wait = 0
+        for material, cost, robots in zip(
+            materials, self.robot_costs[robot_type], number_robots
+        ):
+            if cost == 0 or material >= cost:
+                continue
+            if robots == 0:
+                return -1  # Cannot get enough materials by waiting
+            turns_to_wait = max(turns_to_wait, ceil((cost - material) / robots))
+        if turns_to_wait < time_remaining - 1:
+            return turns_to_wait
+        return -1  # Time to purchase longer than time remaining
 
     def achievable(
         self, time_remaining: int, number_robots: list[int], materials: list[int]
@@ -92,45 +100,54 @@ class Blueprint:
         time_remaining: int,
         materials: list[int],
         number_robots: list[int],
+        robot_to_build: int | None,
         max_geodes: int,
         cache: set[tuple[int]],
     ) -> tuple[int, set[tuple[int]]]:
+
+        if robot_to_build is not None:
+            time_remaining -= 1
+            materials = self.update_materials(materials, number_robots, 1)
+            materials, number_robots = self.purchase_robot(
+                robot_to_build, number_robots, materials
+            )
+
+        if self.achievable(time_remaining, number_robots, materials) <= max_geodes:
+            return max_geodes, cache
+
         cache_element = tuple([time_remaining] + materials + number_robots)
         if cache_element in cache:
             return max_geodes, cache
         cache.add(cache_element)
-        if self.achievable(time_remaining, number_robots, materials) <= max_geodes:
-            return max_geodes, cache
-        to_purchase_this_round = self.enough_materials(
-            materials, number_robots, time_remaining
-        )
-        materials = self.update_materials(materials, number_robots)
-        if time_remaining == 1:
-            return materials[3], cache
-        geodes, cache = self.next_step(
-            time_remaining - 1, materials, number_robots, max_geodes, cache
-        )
-        max_geodes = max(max_geodes, geodes)
-        # If no purchase
-        for (
-            robot_type
-        ) in (
-            to_purchase_this_round
-        ):  # For the possible purchases. Later robots are purchased first.
+
+        for robot_type in range(len(materials) - 1, -1, -1):
+            time_to_wait = self.turns_to_purchase(
+                materials, number_robots, time_remaining, robot_type
+            )
+            if time_to_wait == -1:
+                continue
             geodes, cache = self.next_step(
-                time_remaining - 1,
-                *self.purchase_robot(robot_type, number_robots, materials),
+                time_remaining - time_to_wait,
+                self.update_materials(materials, number_robots, time_to_wait),
+                copy(number_robots),
+                robot_type,
                 max_geodes,
                 cache,
             )
             max_geodes = max(geodes, max_geodes)
+
+        # We do not purchase until the end
+        geodes = materials[3] + number_robots[3] * time_remaining
+        max_geodes = max(geodes, max_geodes)
         return max_geodes, cache
 
     def max_geodes(self, time_limit: int) -> int:
         number_robots = [1, 0, 0, 0]
         materials = [0, 0, 0, 0]
         cache = set()
-        max_geodes, _ = self.next_step(time_limit, materials, number_robots, 0, cache)
+        max_geodes, _ = self.next_step(
+            time_limit, materials, number_robots, None, 0, cache
+        )
         return max_geodes
 
     def quality_level(self, time_limit: int) -> int:
@@ -168,12 +185,7 @@ if __name__ == "__main__":
         file_name = sys.argv[1]
     except IndexError:
         file_name = "input.txt"
-    blueprints = open(file_name).read().split("\n")
-    try:
-        blueprints.remove("")
-    except ValueError:
-        pass
-
+    blueprints = open(file_name).read().splitlines()
     blueprints = list(map(Blueprint, blueprints))
 
     print(f"The total quality level is {multiprocess_quality_levels(blueprints)}.")
